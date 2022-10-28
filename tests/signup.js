108946abdc39a241c165e159cb5af6e10c537aa4
Tiago Oliveira <tiagooliveira@bemol.com.br>
2022-10-28 10:31:53 -0400

    Smoke test performance

---
+++ tests/signup.js
@@ -0,0 +1,31 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+
+import uuid from './libs/uuid.js'
+
+export const options = {
+    vus: 10,
+    duration: '30s',
+    thresholds: {
+        http_req_duration: ['p(95)<2000'], // 95% devem ser em ate 2 segundos   }
+        http_req_failed: ['rate<0.01'] // 1% das request podem ocorrer erro
+    }
+}
+
+export default function () {
+    const url = 'http://localhost:3333/signup'
+    const payload = JSON.stringify(
+        { email: `${uuid.v4().substring(24)}@connections.com`, password: 'UmDois12' }
+    )
+    const headers = {
+        'headers': { 'Content-Type': 'application/json' }
+    }
+    const res = http.post(url, payload, headers)
+
+    console.log(res.body)
+
+    check(res, {
+        'status should be 201': (r) => r.status === 201
+    })
+    sleep(1)
+}
